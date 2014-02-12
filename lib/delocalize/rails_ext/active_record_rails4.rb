ef7b454df43eb259d10da9a86cb0078bc68ba11e
Joao Paulo <joaopaulodesousa@gmail.com>
2014-02-12 18:53:28 -0300

    verifies that the field has been modified, and adjusts according to the location.

--- lib/delocalize/rails_ext/active_record_rails4.rb
+++ lib/delocalize/rails_ext/active_record_rails4.rb
@@ -26,4 +26,30 @@ ActiveRecord::ConnectionAdapters::Column.class_eval do
   end
 
   alias_method_chain :type_cast_for_write, :localization
+
+
+  ActiveRecord::Base.class_eval do
+    def convert_number_column_value_with_localization(value)
+      value = Numeric.parse_localized(value) if I18n.delocalization_enabled?
+      value
+    end
+
+    define_method( :_field_changed? ) do |attr, old, value|
+      if column = column_for_attribute(attr)
+        if column.number? && column.null && (old.nil? || old == 0) && value.blank?
+          # For nullable numeric columns, NULL gets stored in database for blank (i.e. '') values.
+          # Hence we don't record it as a change if the value changes from nil to ''.
+          # If an old value of 0 is set to '' we want this to get changed to nil as otherwise it'll
+          # be typecast back to 0 (''.to_i => 0)
+          value = nil
+        elsif column.number?
+          value = column.type_cast(convert_number_column_value_with_localization(value))
+        else
+          value = column.type_cast(value)
+        end
+      end
+      old != value
+    end
+  end
+
 end
